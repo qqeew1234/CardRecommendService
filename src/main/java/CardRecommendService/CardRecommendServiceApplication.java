ed0d7b364737bf30c7990da6a204b4d8ae5af9f9
qqeew1234 <qqeew1234@gmail.com>
2025-03-10 11:49:05 +0900

    최초

---
+++ src/main/java/CardRecommendService/CardRecommendServiceApplication.java
@@ -0,0 +1,13 @@
+package CardRecommendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardRecommendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardRecommendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CardRecommendService/CardRecommendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package CardRecommendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardRecommendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
