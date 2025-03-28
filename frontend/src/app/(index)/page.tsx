7f755fce62d348d65ec74c8b564ed26cc88c30b5
moonsunghee <loewy.moon@gmail.com>
2025-03-28 20:29:55 +0900

    page05 fin

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,29 +1,28 @@
 "use client";
 
-import CreditCard from "@/components/CreditCard";
 import PageHeader from "@/components/PageHeader";
 import "@/styles/page01.scss";
 import "@/styles/popup.scss";
-import Image from "next/image";
-import Link from "next/link";
+import styles from "@/styles/CreditCard.module.scss";
 import { useEffect, useState } from "react";
 import { FaTimesCircle } from "react-icons/fa";
 import { KakaoLoginButton } from "@/components/kakaoLogin";
 import { GoogleLoginButton } from "@/components/googleLogin";
 import { GitHubLoginButton } from "@/components/gitHubLogin";
 import { createClient } from "@/utils/supabase/client";
+import Link from "next/link";
 
 export default function Home() {
   const [isPopup, setIsPopup] = useState(false);
   const [displayName, setDisplayName] = useState("");
-  const isLoginFail = false;
+  const [isFlipped, setIsFlipped] = useState(false); // 카드 플립 상태 추가
   const supabase = createClient();
+
   useEffect(() => {
     async function getDisplayName() {
       const { data } = await supabase.auth.getUser();
-      const displayName =
-        data?.user?.user_metadata?.full_name || data?.user?.email;
-      setDisplayName(displayName);
+      const name = data?.user?.user_metadata?.full_name || data?.user?.email;
+      setDisplayName(name);
     }
 
     getDisplayName();
@@ -34,6 +33,7 @@ export default function Home() {
     tit: "Card Fit",
     des: "사용자의 소비 패턴, 라이프스타일, 금융 목표에 기반한 최적의 신용카드 추천, 복잡한 카드 혜택 비교를 간소화하여 누구나 쉽게 자신에게 맞는 카드를 선택 가능",
   };
+
   return (
     <>
       <PageHeader
@@ -43,6 +43,7 @@ export default function Home() {
       >
         <></>
       </PageHeader>
+
       <div className="page-body p01">
         <section>
           <div className="art-box">
@@ -50,85 +51,85 @@ export default function Home() {
               <h4>소비 패턴에 따른 카드 추천</h4>
               <p>
                 사용자의 소비 패턴, 라이프스타일, 금융 목표에 기반한 최적의
-                신용카드 추천 복잡한 카드 혜택 비교를 간소화하여 누구나 쉽게
-                자신에게 맞는 카드를 선택 가능 신뢰도 높은 데이터 기반 추천으로
-                사용자 만족도 극대화
+                신용카드 추천. 복잡한 카드 혜택 비교를 간소화하여 누구나 쉽게
+                자신에게 맞는 카드를 선택 가능.
               </p>
             </article>
             <article>
               <h4>고객 취향에 맞춘 혜택</h4>
               <p>
                 사용자의 소비 패턴, 라이프스타일, 금융 목표에 기반한 최적의
-                신용카드 추천 복잡한 카드 혜택 비교를 간소화하여 누구나 쉽게
-                자신에게 맞는 카드를 선택 가능 신뢰도 높은 데이터 기반 추천으로
-                사용자 만족도 극대화
-              </p>
-            </article>
-            <article>
-              <h4>카드 선택에 어려움을 격는 고객을 위한</h4>
-              <p>
-                사용자의 소비 패턴, 라이프스타일, 금융 목표에 기반한 최적의
-                신용카드 추천 복잡한 카드 혜택 비교를 간소화하여 누구나 쉽게
-                자신에게 맞는 카드를 선택 가능 신뢰도 높은 데이터 기반 추천으로
-                사용자 만족도 극대화
+                신용카드 추천. 신뢰도 높은 데이터 기반 추천으로 사용자 만족도
+                극대화.
               </p>
             </article>
           </div>
+
           <div className="img-box">
-            {displayName ? (
-              <div className="user-box">
-                <h4 className="btn-top">
-                  {displayName}님
-                  <button
-                    className=""
-                    onClick={async () => {
-                      await supabase.auth.signOut();
-                      setDisplayName("");
-                    }}
-                  >
-                    로그아웃
-                  </button>
-                </h4>
-                <div className="btn-bot">
-                  <Link href={"/page02"}>
-                    <button className={"active"}>소비패턴 분석하기</button>
-                  </Link>
+            {/* 클릭 시 카드 플립 */}
+            <div
+              className={`${styles["credit-card"]} ${
+                isFlipped ? styles.flipped : ""
+              }`}
+              onClick={() => setIsFlipped(!isFlipped)}
+            >
+              <div className={styles["card-inner"]}>
+                {/* 카드 앞면 */}
+                <div className={styles["card-front"]}>
+                  <div className={styles["card-brand"]}>
+                    <div>Card Fit</div>
+                    <div>ㄹㄹㄹㄹ</div>
+                  </div>
+                  {displayName ? (
+                    <div className="user-box">
+                      <div className="btn-bot">
+                        <Link href={"/page02"}>
+                          <button className="active">소비패턴 분석하기</button>
+                        </Link>
+                      </div>
+                    </div>
+                  ) : (
+                    <button
+                      className="active"
+                      onClick={() => setIsFlipped(true)}
+                    >
+                      로그인
+                    </button>
+                  )}
+                  <div className={styles["card-holder"]}>
+                    <div className={styles["name"]}>{displayName}님</div>
+                    <div className={styles["expiry"]}>EXP</div>
+                  </div>
+                </div>
+
+                {/* 카드 뒷면 */}
+                <div className={styles["card-back"]}>
+                  <div className={styles["cvv"]}>
+                    <form action="">
+                      <button
+                        onClick={async () => {
+                          await supabase.auth.signOut();
+                          setDisplayName("");
+                        }}
+                      >
+                        로그아웃
+                      </button>
+                      <KakaoLoginButton />
+                      <GoogleLoginButton />
+                      <GitHubLoginButton />
+                    </form>
+                  </div>
                 </div>
               </div>
-            ) : (
-              <button className={"active"} onClick={() => setIsPopup(true)}>
-                로그인
-              </button>
-            )}
-            <CreditCard
-              cardNumber="1234 5678 9012 3456"
-              cardHolder={displayName}
-              expiryDate="12/26"
-              cvv="123"
-              brand="visa"
-            />
+            </div>
           </div>
         </section>
       </div>
+
       {isPopup && (
         <div className="popup-screen">
           <div className="popup-box">
             <form action="">
-              {/* <input
-                type="email"
-                className="email"
-                placeholder="example@cardfit.com"
-              />
-              <input
-                type="password"
-                className="password"
-                placeholder="비밀번호를 입력 하세요."
-              />
-              <button className="login">이메일 로그인</button>
-              <Link href={"/signup"}>
-                <button className="signup">[회원가입하기]</button>
-              </Link> */}
-
               <KakaoLoginButton />
               <GoogleLoginButton />
               <GitHubLoginButton />
@@ -136,14 +137,6 @@ export default function Home() {
             <div className="close-btn" onClick={() => setIsPopup(false)}>
               <FaTimesCircle />
             </div>
-            {isLoginFail && (
-              <div className="isLoginFail">
-                <h4>이메일 또는 비밀번호를 다시 확인해 주세요.</h4>
-                <div className="btns">
-                  <button>닫기</button>
-                </div>
-              </div>
-            )}
           </div>
         </div>
       )}
--- frontend/src/app/page05/page.tsx
+++ frontend/src/app/page05/page.tsx
@@ -1,5 +1,7 @@
 import PageHeader from "@/components/PageHeader05";
+import testData from "@/json/cardHistoryResponses.json";
 import Link from "next/link";
+import "@/styles/page05.scss";
 export default function page05() {
   const hdProps = {
     num: "05",
@@ -13,36 +15,71 @@ export default function page05() {
       { cardCorp: "신한카드", cardName: "내일배움" },
     ],
   };
+  const testCard = testData.cardHistoryResponses;
   return (
     <>
-      <PageHeader
-        num={hdProps.num}
-        years={hdProps.years}
-        months={hdProps.months}
-        cardList={hdProps.cardList}
-      >
-        <Link href={"/page04"}>
-          <button>카드다시 선택하기</button>
-        </Link>
-        <Link href={"/page06"}>
-          <button className="active">소비패턴 분석하기</button>
-        </Link>
-      </PageHeader>
+      <div className="page-header">
+        <PageHeader
+          num={hdProps.num}
+          years={hdProps.years}
+          months={hdProps.months}
+          cardList={hdProps.cardList}
+        >
+          <Link href={"/page04"}>
+            <button>카드다시 선택하기</button>
+          </Link>
+          <Link href={"/page06"}>
+            <button className="active">소비패턴 분석하기</button>
+          </Link>
+        </PageHeader>
+        <div className="list-header">
+          <div className="list-item list-item-01">이용일</div>
+          <div className="list-item list-item-02">이용내역</div>
+          <div className="list-item list-item-03">이용금액</div>
+          <div className="list-item list-item-04">일별이용금액</div>
+        </div>
+      </div>
       <div className="page-body p05">
         <section>
-          <div className="list-header">
-            <div className="list-item day">이용일</div>
-            <div className="list-item des">이용내역</div>
-            <div className="list-item pay">이용금액</div>
-            <div className="list-item total">일별이용금액</div>
-          </div>
-          <div className="list-row">
-            <div className="list-item day"></div>
-            <div className="list-item des"></div>
-            <div className="list-item pay"></div>
-            <div className="list-item total"></div>
-          </div>
+          {testCard.map((card, index) => (
+            <div className="list-row" key={index}>
+              <div className="list-item list-item-01">
+                <b>{card.date}</b> &nbsp;일
+              </div>
+              <div className="list-item list-item-02">
+                {card.paymentHistories.map((usage, i) => (
+                  <div className="usageLog" key={i}>
+                    <h4>{usage.storeName}</h4>
+                    <p>
+                      {usage.cardName},{usage.paymentDatetime},{"결제형태"}
+                    </p>
+                  </div>
+                ))}
+              </div>
+              <div className="list-item list-item-03">
+                {card.paymentHistories.map((payment, i) => (
+                  <div className="payment" key={i}>
+                    <b>{payment.amount.toLocaleString()}</b>원
+                  </div>
+                ))}
+              </div>
+              <div className="list-item list-item-04 dayTotalPayment">
+                <b>{card.dailyAmount.toLocaleString()}</b>원
+              </div>
+            </div>
+          ))}
+          <button className="moreBtn">더보기</button>
         </section>
+        <footer>
+          <div className="total">
+            <div className="totalCount">
+              총<b>{testData.totalCount.toLocaleString()}</b>건
+            </div>
+            <div className="totalCost">
+              <b>{testData.totalCost.toLocaleString()}</b>원
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
